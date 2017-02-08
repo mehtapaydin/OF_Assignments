5d5009deb15f5d5699372435a216dee545f74a17
mehtapaydin <mehtapaydinnnn@gmail.com>
2017-02-08 14:52:13 -0500

    Week3 Assignment

---
+++ Assignment_Week3/src/ofApp.cpp
@@ -0,0 +1,464 @@
+#include "ofApp.h"
+ofPolyline curvedSegmentPolyline;
+ofPolyline curvedSegmentPolyline0;
+ofPolyline curvedSegmentPolyline1;
+ofPolyline curvedSegmentPolyline2;
+ofPolyline curvedSegmentPolyline3;
+ofPolyline curvedSegmentPolyline4;
+ofPolyline curvedSegmentPolyline5;
+ofPolyline curvedSegmentPolyline6;
+
+ofPolyline curvedSegmentPolyline7;
+ofPolyline curvedSegmentPolyline8;
+ofPolyline curvedSegmentPolyline9;
+ofPolyline curvedSegmentPolyline10;
+ofPolyline curvedSegmentPolyline11;
+ofPolyline curvedSegmentPolyline12;
+ofPolyline curvedSegmentPolyline13;
+ofPolyline curvedSegmentPolyline14;
+
+
+ofPolyline curvedSegmentPolyline15;
+ofPolyline curvedSegmentPolyline16;
+ofPolyline curvedSegmentPolyline17;
+ofPolyline curvedSegmentPolyline18;
+ofPolyline curvedSegmentPolyline19;
+ofPolyline curvedSegmentPolyline20;
+ofPolyline curvedSegmentPolyline21;
+ofPolyline curvedSegmentPolyline22;
+ofPolyline curvedSegmentPolyline23;
+ofPolyline curvedSegmentPolyline24;
+
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    ofSetWindowShape(800, 800);
+    ofSetWindowPosition(10, 10);
+
+
+    curvedSegmentPolyline.curveTo(0, 371);
+    curvedSegmentPolyline.curveTo(0, 371);
+    curvedSegmentPolyline.curveTo(143, 291);
+    curvedSegmentPolyline.curveTo(306, 358);
+    curvedSegmentPolyline.curveTo(528, 302);
+    curvedSegmentPolyline.curveTo(744, 370);
+    curvedSegmentPolyline.curveTo(800, 371);
+    curvedSegmentPolyline.curveTo(800, 371);
+    
+    curvedSegmentPolyline0.curveTo(0, 376);
+    curvedSegmentPolyline0.curveTo(0, 376);
+    curvedSegmentPolyline0.curveTo(143, 306);
+    curvedSegmentPolyline0.curveTo(306, 363);
+    curvedSegmentPolyline0.curveTo(528, 307);
+    curvedSegmentPolyline0.curveTo(744, 385);
+    curvedSegmentPolyline0.curveTo(800, 376);
+    curvedSegmentPolyline0.curveTo(800, 376);
+  
+    
+    curvedSegmentPolyline1.curveTo(0, 381);
+    curvedSegmentPolyline1.curveTo(0, 381);
+    curvedSegmentPolyline1.curveTo(143, 321);
+    curvedSegmentPolyline1.curveTo(306, 368);
+    curvedSegmentPolyline1.curveTo(528, 312);
+    curvedSegmentPolyline1.curveTo(744, 400);
+    curvedSegmentPolyline1.curveTo(800, 381);
+    curvedSegmentPolyline1.curveTo(800, 381);
+    
+    curvedSegmentPolyline2.curveTo(0, 386);
+    curvedSegmentPolyline2.curveTo(0, 386);
+    curvedSegmentPolyline2.curveTo(143, 336);
+    curvedSegmentPolyline2.curveTo(306, 373);
+    curvedSegmentPolyline2.curveTo(528, 317);
+    curvedSegmentPolyline2.curveTo(744, 415);
+    curvedSegmentPolyline2.curveTo(800, 386);
+    curvedSegmentPolyline2.curveTo(800, 386);
+    
+    curvedSegmentPolyline3.curveTo(0, 391);
+    curvedSegmentPolyline3.curveTo(0, 391);
+    curvedSegmentPolyline3.curveTo(143, 351);
+    curvedSegmentPolyline3.curveTo(306, 378);
+    curvedSegmentPolyline3.curveTo(528, 322);
+    curvedSegmentPolyline3.curveTo(744, 430);
+    curvedSegmentPolyline3.curveTo(800, 391);
+    curvedSegmentPolyline3.curveTo(800, 391);
+    
+    curvedSegmentPolyline4.curveTo(0, 396);
+    curvedSegmentPolyline4.curveTo(0, 396);
+    curvedSegmentPolyline4.curveTo(143, 366);
+    curvedSegmentPolyline4.curveTo(306, 383);
+    curvedSegmentPolyline4.curveTo(528, 327);
+    curvedSegmentPolyline4.curveTo(744, 445);
+    curvedSegmentPolyline4.curveTo(800, 396);
+    curvedSegmentPolyline4.curveTo(800, 396);
+    
+    curvedSegmentPolyline5.curveTo(0, 401);
+    curvedSegmentPolyline5.curveTo(0, 391);
+    curvedSegmentPolyline5.curveTo(143, 381);
+    curvedSegmentPolyline5.curveTo(306, 388);
+    curvedSegmentPolyline5.curveTo(528, 332);
+    curvedSegmentPolyline5.curveTo(744, 460);
+    curvedSegmentPolyline5.curveTo(800, 401);
+    curvedSegmentPolyline5.curveTo(800, 401);
+    
+    curvedSegmentPolyline6.curveTo(0, 406);
+    curvedSegmentPolyline6.curveTo(0, 406);
+    curvedSegmentPolyline6.curveTo(143, 396);
+    curvedSegmentPolyline6.curveTo(306, 393);
+    curvedSegmentPolyline6.curveTo(528, 337);
+    curvedSegmentPolyline6.curveTo(744, 475);
+    curvedSegmentPolyline6.curveTo(800, 406);
+    curvedSegmentPolyline6.curveTo(800, 406);
+    
+    
+    //.....
+    
+    
+
+    
+    //......
+    curvedSegmentPolyline7.curveTo(0, 371);
+    curvedSegmentPolyline7.curveTo(0, 371);
+    curvedSegmentPolyline7.curveTo(163, 450);
+    curvedSegmentPolyline7.curveTo(326, 391);
+    curvedSegmentPolyline7.curveTo(528, 438);
+    curvedSegmentPolyline7.curveTo(704, 450);
+    curvedSegmentPolyline7.curveTo(800, 351);
+    curvedSegmentPolyline7.curveTo(800, 351);
+    
+    
+    curvedSegmentPolyline8.curveTo(0, 376);
+    curvedSegmentPolyline8.curveTo(0, 376);
+    curvedSegmentPolyline8.curveTo(163, 455);
+    curvedSegmentPolyline8.curveTo(326, 401);
+    curvedSegmentPolyline8.curveTo(528, 443);
+    curvedSegmentPolyline8.curveTo(704, 455);
+    curvedSegmentPolyline8.curveTo(800, 356);
+    curvedSegmentPolyline8.curveTo(800, 356);
+    
+    curvedSegmentPolyline9.curveTo(0, 381);
+    curvedSegmentPolyline9.curveTo(0, 381);
+    curvedSegmentPolyline9.curveTo(163, 460);
+    curvedSegmentPolyline9.curveTo(326, 421);
+    curvedSegmentPolyline9.curveTo(528, 448);
+    curvedSegmentPolyline9.curveTo(704, 460);
+    curvedSegmentPolyline9.curveTo(800, 361);
+    curvedSegmentPolyline9.curveTo(800, 361);
+    
+    
+    curvedSegmentPolyline10.curveTo(0, 386);
+    curvedSegmentPolyline10.curveTo(0, 386);
+    curvedSegmentPolyline10.curveTo(163, 465);
+    curvedSegmentPolyline10.curveTo(326, 431);
+    curvedSegmentPolyline10.curveTo(528, 453);
+    curvedSegmentPolyline10.curveTo(704, 465);
+    curvedSegmentPolyline10.curveTo(800, 366);
+    curvedSegmentPolyline10.curveTo(800, 366);
+    
+    
+    curvedSegmentPolyline11.curveTo(0, 391);
+    curvedSegmentPolyline11.curveTo(0, 391);
+    curvedSegmentPolyline11.curveTo(163, 470);
+    curvedSegmentPolyline11.curveTo(326, 441);
+    curvedSegmentPolyline11.curveTo(528, 458);
+    curvedSegmentPolyline11.curveTo(704, 470);
+    curvedSegmentPolyline11.curveTo(800, 371);
+    curvedSegmentPolyline11.curveTo(800, 371);
+    
+    
+    curvedSegmentPolyline12.curveTo(0, 371);
+    curvedSegmentPolyline12.curveTo(0, 371);
+    curvedSegmentPolyline12.curveTo(163, 475);
+    curvedSegmentPolyline12.curveTo(326, 451);
+    curvedSegmentPolyline12.curveTo(528, 463);
+    curvedSegmentPolyline12.curveTo(704, 475);
+    curvedSegmentPolyline12.curveTo(800, 376);
+    curvedSegmentPolyline12.curveTo(800, 376);
+    
+    curvedSegmentPolyline13.curveTo(0, 371);
+    curvedSegmentPolyline13.curveTo(0, 371);
+    curvedSegmentPolyline13.curveTo(163, 480);
+    curvedSegmentPolyline13.curveTo(326, 461);
+    curvedSegmentPolyline13.curveTo(528, 468);
+    curvedSegmentPolyline13.curveTo(704, 480);
+    curvedSegmentPolyline13.curveTo(800, 381);
+    curvedSegmentPolyline13.curveTo(800, 381);
+    
+    curvedSegmentPolyline14.curveTo(0, 371);
+    curvedSegmentPolyline14.curveTo(0, 371);
+    curvedSegmentPolyline14.curveTo(163, 485);
+    curvedSegmentPolyline14.curveTo(326, 471);
+    curvedSegmentPolyline14.curveTo(528, 473);
+    curvedSegmentPolyline14.curveTo(704, 485);
+    curvedSegmentPolyline14.curveTo(800, 386);
+    curvedSegmentPolyline14.curveTo(800, 386);
+    
+    //....
+    
+    
+    curvedSegmentPolyline15.curveTo(0, 571);
+    curvedSegmentPolyline15.curveTo(0, 571);
+    curvedSegmentPolyline15.curveTo(143, 491);
+    curvedSegmentPolyline15.curveTo(306, 558);
+    curvedSegmentPolyline15.curveTo(528, 502);
+    curvedSegmentPolyline15.curveTo(744, 570);
+    curvedSegmentPolyline15.curveTo(800, 571);
+    curvedSegmentPolyline15.curveTo(800, 571);
+    
+    curvedSegmentPolyline16.curveTo(0, 576);
+    curvedSegmentPolyline16.curveTo(0, 576);
+    curvedSegmentPolyline16.curveTo(143, 506);
+    curvedSegmentPolyline16.curveTo(306, 563);
+    curvedSegmentPolyline16.curveTo(528, 507);
+    curvedSegmentPolyline16.curveTo(744, 585);
+    curvedSegmentPolyline16.curveTo(800, 576);
+    curvedSegmentPolyline16.curveTo(800, 576);
+    
+    
+    curvedSegmentPolyline17.curveTo(0, 581);
+    curvedSegmentPolyline17.curveTo(0, 581);
+    curvedSegmentPolyline17.curveTo(143, 521);
+    curvedSegmentPolyline17.curveTo(306, 568);
+    curvedSegmentPolyline17.curveTo(528, 512);
+    curvedSegmentPolyline17.curveTo(744, 600);
+    curvedSegmentPolyline17.curveTo(800, 581);
+    curvedSegmentPolyline17.curveTo(800, 581);
+    
+    curvedSegmentPolyline18.curveTo(0, 586);
+    curvedSegmentPolyline18.curveTo(0, 586);
+    curvedSegmentPolyline18.curveTo(143, 536);
+    curvedSegmentPolyline18.curveTo(306, 573);
+    curvedSegmentPolyline18.curveTo(528, 517);
+    curvedSegmentPolyline18.curveTo(744, 615);
+    curvedSegmentPolyline18.curveTo(800, 586);
+    curvedSegmentPolyline18.curveTo(800, 586);
+    
+    curvedSegmentPolyline19.curveTo(0, 591);
+    curvedSegmentPolyline19.curveTo(0, 591);
+    curvedSegmentPolyline19.curveTo(143, 551);
+    curvedSegmentPolyline19.curveTo(306, 578);
+    curvedSegmentPolyline19.curveTo(528, 522);
+    curvedSegmentPolyline19.curveTo(744, 630);
+    curvedSegmentPolyline19.curveTo(800, 591);
+    curvedSegmentPolyline19.curveTo(800, 591);
+    
+    curvedSegmentPolyline20.curveTo(0, 596);
+    curvedSegmentPolyline20.curveTo(0, 596);
+    curvedSegmentPolyline20.curveTo(143, 566);
+    curvedSegmentPolyline20.curveTo(306, 583);
+    curvedSegmentPolyline20.curveTo(528, 527);
+    curvedSegmentPolyline20.curveTo(744, 645);
+    curvedSegmentPolyline20.curveTo(800, 596);
+    curvedSegmentPolyline20.curveTo(800, 596);
+    
+    curvedSegmentPolyline21.curveTo(0, 601);
+    curvedSegmentPolyline21.curveTo(0, 591);
+    curvedSegmentPolyline21.curveTo(143, 581);
+    curvedSegmentPolyline21.curveTo(306, 588);
+    curvedSegmentPolyline21.curveTo(528, 532);
+    curvedSegmentPolyline21.curveTo(744, 660);
+    curvedSegmentPolyline21.curveTo(800, 601);
+    curvedSegmentPolyline21.curveTo(800, 601);
+    
+    curvedSegmentPolyline22.curveTo(0, 606);
+    curvedSegmentPolyline22.curveTo(0, 606);
+    curvedSegmentPolyline22.curveTo(143, 596);
+    curvedSegmentPolyline22.curveTo(306, 593);
+    curvedSegmentPolyline22.curveTo(528, 537);
+    curvedSegmentPolyline22.curveTo(744, 675);
+    curvedSegmentPolyline22.curveTo(800, 606);
+    curvedSegmentPolyline22.curveTo(800, 606);
+    
+    curvedSegmentPolyline23.curveTo(0, 611);
+    curvedSegmentPolyline23.curveTo(0, 611);
+    curvedSegmentPolyline23.curveTo(143, 616);
+    curvedSegmentPolyline23.curveTo(306, 593);
+    curvedSegmentPolyline23.curveTo(528, 542);
+    curvedSegmentPolyline23.curveTo(744, 680);
+    curvedSegmentPolyline23.curveTo(800, 611);
+    curvedSegmentPolyline23.curveTo(800, 611);
+    
+    curvedSegmentPolyline24.curveTo(0, 616);
+    curvedSegmentPolyline24.curveTo(0, 616);
+    curvedSegmentPolyline24.curveTo(143, 626);
+    curvedSegmentPolyline24.curveTo(306, 598);
+    curvedSegmentPolyline24.curveTo(528, 547);
+    curvedSegmentPolyline24.curveTo(744, 685);
+    curvedSegmentPolyline24.curveTo(800, 616);
+    curvedSegmentPolyline24.curveTo(800, 616);
+    
+    
+
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    
+    ofColor colorOne(57, 109, 245);
+    ofColor colorTwo(13, 130, 247);
+    ofBackgroundGradient(colorTwo, colorOne, OF_GRADIENT_LINEAR);
+
+    ofSetColor(255,255,255);
+    
+    
+    //Horizon
+    
+    for (int i=0; i < 300;  i+=5) {
+
+        ofDrawLine(0, i, 800, i);
+        ofDrawRectangle(21, 164, 50, 80);
+        ofDrawRectangle(21, 164, 50, 80);
+        
+        ofSetColor(255,255,255);
+        ofFill();
+        ofDrawRectangle(300, 164, 60, 130);
+        ofDrawRectangle(68, 180, 48, 120);
+        ofDrawRectangle(166, 200, 50, 80);
+        ofSetColor(57,109,245);
+        ofFill();
+        ofDrawRectangle(480, 164, 50, 130);
+        ofDrawRectangle(561, 164, 50, 130);
+        ofDrawRectangle(140, 164, 60, 130);
+        ofNoFill();
+        ofDrawRectangle(88, 180, 48, 130);
+        ofDrawRectangle(136, 200, 50, 80);
+        ofSetColor(255,255,255);
+        ofDrawRectangle(780, 164, 60, 130);
+        ofDrawRectangle(500, 180, 48, 120);
+        ofFill();
+        ofDrawRectangle(470, 200, 50, 80);
+        ofSetColor(57,109,245);
+        ofFill();
+        ofDrawRectangle(480, 164, 50, 130);
+        ofDrawRectangle(561, 164, 50, 130);
+        ofDrawRectangle(140, 164, 60, 130);
+        ofSetColor(127,255,145);
+        ofNoFill();
+        ofDrawRectangle(200, 180, 48, 130);
+        ofDrawRectangle(346, 200, 50, 80);
+        ofDrawRectangle(540, 164, 50, 130);
+        ofDrawRectangle(561, 164, 50, 130);
+        ofFill();
+        ofDrawRectangle(140, 164, 60, 130);
+        ofDrawRectangle(240, 180, 48, 130);
+        ofDrawRectangle(236, 200, 50, 80);
+        ofDrawRectangle(417, 164, 50, 130);
+        ofDrawRectangle(531, 164, 50, 130);
+        ofSetColor(255,255,255);
+    }
+    
+    //Buildings
+    
+    if (ofGetMousePressed(OF_MOUSE_BUTTON_LEFT)) {
+     
+        float randomColor = ofRandom(50, 255);
+        ofSetColor(randomColor);
+        ofDrawRectangle(300, 164, 60, 130);
+        ofDrawRectangle(346, 200, 50, 80);
+        ofDrawRectangle(540, 164, 50, 130);
+        
+    
+    // If the right mouse button is pressed...
+    if (ofGetMousePressed(OF_MOUSE_BUTTON_RIGHT)) {
+        
+        ofColor colorOne(57, 109, 245);
+        ofColor colorTwo(13, 130, 247);
+        ofBackgroundGradient(colorTwo, colorOne, OF_GRADIENT_LINEAR);
+    }
+        
+ }
+    
+    
+    // Sea
+    ofSetColor(255,255,255);
+    ofSetLineWidth(1.6);
+    curvedSegmentPolyline.draw();
+    curvedSegmentPolyline0.draw();
+    curvedSegmentPolyline1.draw();
+    curvedSegmentPolyline2.draw();
+    curvedSegmentPolyline3.draw();
+    curvedSegmentPolyline4.draw();
+    curvedSegmentPolyline5.draw();
+    curvedSegmentPolyline6.draw();
+    
+    
+    curvedSegmentPolyline7.draw();
+    curvedSegmentPolyline8.draw();
+    curvedSegmentPolyline9.draw();
+    curvedSegmentPolyline10.draw();
+    curvedSegmentPolyline11.draw();
+    curvedSegmentPolyline12.draw();
+    curvedSegmentPolyline13.draw();
+    curvedSegmentPolyline14.draw();
+    
+    curvedSegmentPolyline15.draw();
+    curvedSegmentPolyline16.draw();
+    curvedSegmentPolyline17.draw();
+    curvedSegmentPolyline18.draw();
+    curvedSegmentPolyline19.draw();
+    curvedSegmentPolyline20.draw();
+    curvedSegmentPolyline21.draw();
+    curvedSegmentPolyline22.draw();
+    curvedSegmentPolyline23.draw();
+    curvedSegmentPolyline24.draw();
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
